e118dedfecfd4faeb67bd69afab677df8aa80adc
Luigui Delyer <git@s1x.com.br>
2024-07-25 14:00:27 +0200

    Bumping version and changing the esbuild configuration

--- esbuild.js
+++ esbuild.js
@@ -1,15 +1,11 @@
 const { build } = require('esbuild');
-const { dependencies } = require('./package.json')
-
-const sharedConfig = {
-    entryPoints: ['src/index.ts'],
-    bundle: true,
-    minify: true,
-    external: Object.keys(dependencies),
-}
 
 build({
-    ...sharedConfig,
+    entryPoints: ['src/index.ts'],
+    outfile: "dist/index.js",
+    bundle: true,
+    minify: true,
     platform: 'node',
-    outfile: 'dist/index.js',
+    target: 'node18',
+    format: "esm",
 })

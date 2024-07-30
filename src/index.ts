7b1989e8ab2fec96e1e497f31a09170afc7b6bb2
Luigui Delyer <git@s1x.com.br>
2024-07-30 14:09:32 +0200

    Publishing new version

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import libnpmexec from 'libnpmexec'
 import { BasedClient } from '@based/client'
 import { readFileSync, existsSync } from 'fs'
 import { join } from 'path'
-// import { exec } from 'child_process'
-// import { promisify } from 'util'
+import { exec } from 'child_process'
+import { promisify } from 'util'
 import { wait } from '@saulx/utils'
 
-// const execPromise = promisify(exec)
+const execPromise = promisify(exec)
 
 async function run() {
   try {
@@ -95,15 +94,10 @@ async function run() {
 
     core.info('☁️ Starting the Deploy using the Based CLI...')
 
-    const cli = await libnpmexec({
-      args: ['@based/cli', 'deploy', '--api-key', token],
-      yes: true,
-    })
-    core.info(`💬 CLI: ${cli}`)
-
-    // const { stdout, stderr } = await execPromise(`npx @based/cli deploy --api-key "${token}"`)
-    // core.info(`💬 stdout: ${stdout}`)
-    // core.error(`💬 stderr: ${stderr}`)
+    const cmd = `npx @based/cli deploy --api-key "${token}"`
+    const { stdout, stderr } = await execPromise(cmd)
+    core.info(`💬 stdout: ${stdout}`)
+    core.error(`💬 stderr: ${stderr}`)
 
     core.setOutput('response', '🎉 Success! Enjoy your fastest deploy ever!')
   } catch (error) {

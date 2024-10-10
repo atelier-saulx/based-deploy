0ad29d8ba2dc4c706b46bc103c4c2eaa0f112544
Luigui Delyer <git@s1x.com.br>
2024-10-10 22:28:46 +0200

    Publishing new version

--- esbuild.cjs
+++ esbuild.cjs
@@ -1,11 +1,12 @@
-const { build } = require('esbuild');
+const { build } = require('esbuild')
 
 build({
-    entryPoints: ['src/index.ts'],
-    outfile: "dist/index.cjs",
-    bundle: true,
-    minify: false,
-    platform: 'node',
-    target: 'node18',
-    format: "cjs",
+  entryPoints: ['src/index.ts'],
+  outfile: 'dist/index.cjs',
+  bundle: true,
+  minify: false,
+  platform: 'node',
+  target: 'node18',
+  format: 'cjs',
+  external: ['esbuild'],
 })

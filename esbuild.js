6e8e2a8239e122cfbd75b782ca89f261694cc0aa
Luigui Delyer <git@s1x.com.br>
2024-10-11 09:53:34 +0200

    Publishing new version

---
+++ esbuild.js
@@ -0,0 +1,12 @@
+const { build } = require('esbuild')
+
+build({
+  entryPoints: ['src/index.ts'],
+  outfile: 'dist/index.cjs',
+  bundle: true,
+  minify: true,
+  sourcemap: false,
+  platform: 'node',
+  target: 'node18',
+  external: ['esbuild'],
+})
